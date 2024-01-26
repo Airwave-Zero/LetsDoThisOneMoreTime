d1b2042a03e6fe68b6f3656bb3dcd02001a1fda5
Gabriel Quach <gabriel.quach9@gmail.com>
2024-01-25 23:27:05 -0800

    Reorganize json file structure and add file reading code to populate worker form with json data

--- shift_scheduler_bot/SchedulerFormApp/WorkerForm.Designer.cs
+++ shift_scheduler_bot/SchedulerFormApp/WorkerForm.Designer.cs
@@ -29,13 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.WorkerName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Experience = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Busy = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Notes = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -44,14 +37,6 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.WorkerName,
-            this.Age,
-            this.Status,
-            this.Experience,
-            this.Phone,
-            this.Busy,
-            this.Notes});
             this.dataGridView1.Location = new System.Drawing.Point(45, 75);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
@@ -59,62 +44,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(930, 404);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Name
-            // 
-            this.WorkerName.HeaderText = "Full Name";
-            this.WorkerName.MinimumWidth = 6;
-            this.WorkerName.Name = "Name";
-            this.WorkerName.ReadOnly = true;
-            this.WorkerName.Width = 125;
-            // 
-            // Age
-            // 
-            this.Age.HeaderText = "Age";
-            this.Age.MinimumWidth = 6;
-            this.Age.Name = "Age";
-            this.Age.ReadOnly = true;
-            this.Age.Width = 125;
-            // 
-            // Status
-            // 
-            this.Status.HeaderText = "Status";
-            this.Status.MinimumWidth = 6;
-            this.Status.Name = "Status";
-            this.Status.ReadOnly = true;
-            this.Status.Width = 125;
-            // 
-            // Experience
-            // 
-            this.Experience.HeaderText = "Experience";
-            this.Experience.MinimumWidth = 6;
-            this.Experience.Name = "Experience";
-            this.Experience.ReadOnly = true;
-            this.Experience.Width = 125;
-            // 
-            // Phone
-            // 
-            this.Phone.HeaderText = "Phone";
-            this.Phone.MinimumWidth = 6;
-            this.Phone.Name = "Phone";
-            this.Phone.ReadOnly = true;
-            this.Phone.Width = 125;
-            // 
-            // Busy
-            // 
-            this.Busy.HeaderText = "Busy";
-            this.Busy.MinimumWidth = 6;
-            this.Busy.Name = "Busy";
-            this.Busy.ReadOnly = true;
-            this.Busy.Width = 125;
-            // 
-            // Notes
-            // 
-            this.Notes.HeaderText = "Notes";
-            this.Notes.MinimumWidth = 6;
-            this.Notes.Name = "Notes";
-            this.Notes.ReadOnly = true;
-            this.Notes.Width = 125;
             // 
             // button1
             // 
@@ -142,8 +71,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Worker Form";
+            this.Name = "WorkerForm";
             this.Text = "WorkerForm";
+            this.Load += new System.EventHandler(this.WorkerForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -152,13 +82,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn WorkerName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Experience;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Busy;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Notes;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
     }
--- shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
+++ shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace scheduler_test1
 {
@@ -21,5 +25,20 @@ namespace scheduler_test1
         {
 
         }
+
+        private void WorkerForm_Load(object sender, EventArgs e)
+        {
+            string jsonString = File.ReadAllText("C:/Users/bboyf/OneDrive/Desktop/CODE/LetsDoThisOneMoreTime/shift_scheduler_bot/SchedulerFormApp/JSON_files/Workers.json");
+            dataGridView1.DataSource = JsonConvert.DeserializeObject<DataTable>(jsonString);
+        }
     }
+}
+    public class Employee
+{
+    public int age;
+    public string status;
+    public string experience;
+    public string phone;
+    public string[] notes;
+    public string[] busy;
 }
