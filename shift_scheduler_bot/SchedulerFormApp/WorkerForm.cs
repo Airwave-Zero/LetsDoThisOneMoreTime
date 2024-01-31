420097dbfb5b032d4dd892f662394d584b62b0c9
Gabriel Quach <gabriel.quach9@gmail.com>
2024-01-31 00:24:41 -0800

    Implemented import/export functionality for workers and saving editing workers

--- shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
+++ shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
@@ -25,7 +25,7 @@ namespace scheduler_test1
             MessageBox.Show("Attempted to load in: " + fileName);
             if (fileName == "")
             {
-                MessageBox.Show("No file detected, loading in default");
+                //MessageBox.Show("No file detected, loading in default");
                 workerFileName = "C:/Users/bboyf/OneDrive/Desktop/CODE/LetsDoThisOneMoreTime/shift_scheduler_bot/SchedulerFormApp/JSON_files/Workers.json";
             }
             workerFileRaw = File.ReadAllText(workerFileName);
@@ -52,28 +52,30 @@ namespace scheduler_test1
         {
             // Save the current data in the table back into the file
             string output = JsonConvert.SerializeObject(dataGridView1.DataSource);
-            Stream myStream;
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
 
             saveFileDialog1.Filter = "JSON files (*.JSON)|*.JSON|All files (*.*)|*.*";
-            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.FilterIndex = 1;
             saveFileDialog1.RestoreDirectory = true;
 
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                if (saveFileDialog1.FileName != "")
                 {
-                    //filename is saveFileDialog1.FileName
-                    System.IO.File.WriteAllText(saveFileDialog1.FileName, output);
-                    myStream.Close();
+                    File.WriteAllText(saveFileDialog1.FileName, output);
                 }
+            }
+            else
+            {
+                MessageBox.Show("User cancelled save.");
             }
             
         }
         
         private void CancelButton_Click(object sender, EventArgs e)
         {
-            // Close the window, do not update the file
+            // Simply close the window, do not update the file
+            this.Close();
         }
 
         
