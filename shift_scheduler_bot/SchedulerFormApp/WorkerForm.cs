f9a07c9d05c3f617edda3310473d85a2be5bb796
Gabriel Quach <gabriel.quach9@gmail.com>
2024-01-30 23:48:24 -0800

    Add shared utils class for common functions, and added import/export functionality working with json files as well as basic guardrails

--- shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
+++ shift_scheduler_bot/SchedulerFormApp/WorkerForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,15 @@ namespace scheduler_test1
 {
     public partial class WorkerForm : Form
     {
-        public WorkerForm()
+        public string workerFileName;
+        public string workerFileRaw;
+        public WorkerForm(string fileName = "")
         {
+            workerFileName = fileName;
+            if (fileName != "")
+            { 
+                workerFileRaw = File.ReadAllText(workerFileName); 
+            }
             InitializeComponent();
         }
 
@@ -33,14 +41,35 @@ namespace scheduler_test1
             dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
         }
+        
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Save the current data in the table back into the file
+            string output = JsonConvert.SerializeObject(dataGridView1.DataSource);
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "JSON files (*.JSON)|*.JSON|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    //filename is saveFileDialog1.FileName
+                    System.IO.File.WriteAllText(saveFileDialog1.FileName, output);
+                    myStream.Close();
+                }
+            }
+            
+        }
+        
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Close the window, do not update the file
+        }
+
+        
     }
-}
-    public class Employee
-{
-    public int age;
-    public string status;
-    public string experience;
-    public string phone;
-    public string[] notes;
-    public string[] busy;
 }
