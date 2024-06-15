e28e018440a5bc7bea424db4b471e41a03f049c0
Gabriel Quach <gabriel.quach9@gmail.com>
2024-06-15 12:50:01 -0700

    Started the crying app and laying down basic infrastructure

---
+++ crying_app/crying_app/src/App.tsx
@@ -0,0 +1,41 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Container} from "React-bootstrap"
+import {Routes, Route, Navigate} from "react-router-dom"
+import { NewNote } from "./NewNote"
+
+export type Note = {
+  id:string
+  & NoteData
+}
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id:string
+  label:string
+}
+
+function App() {
+  return (
+    <Container className="my-4">
+    <Routes>
+    <Route path="/" element = {<h1>Default</h1>}></Route>
+    <Route path="/home" element = {<h1>Home</h1>}></Route>
+    <Route path="/new" element = <NewNote></NewNote>></Route>
+    <Route path="/:id">
+      {/* This mini route inside routes basically allows for accessing specific pages or ID's
+      so the example here is http://localhost:5173/1/edit
+      Which pulls up the first index, and you can edit that page's information*/}
+      <Route index element = {<h1>Show</h1>}></Route>
+      <Route path = "edit" element={<h1>Edit</h1>}></Route>
+      </Route>
+    <Route path="*" element = {<Navigate to="/" />}></Route>
+  </Routes>
+  </Container>
+  )
+}
+
+export default App
