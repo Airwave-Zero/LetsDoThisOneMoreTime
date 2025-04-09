adcf0c25597daa79ef7f4813142867ca9dae4ab9
Gabriel Quach <gabriel.quach9@gmail.com>
2025-04-08 22:49:53 -0700

    Move all items

--- extensionblocker/content.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    const links = Array.from(document.querySelectorAll('a')).map(anchor => anchor.href);
-
-    // Send the links to the background script
-    chrome.runtime.sendMessage({ type: "EXTRACTED_LINKS", links });
-})();
