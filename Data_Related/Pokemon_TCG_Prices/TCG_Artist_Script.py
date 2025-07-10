290a25debd3bd2c01ba4249dc94ce013d861e637
Gabriel Quach <gabriel.quach9@gmail.com>
2025-07-10 03:52:29 -0700

    Restructure tcg to make modular

--- Data_Related/Pokemon_TCG_Prices/TCG_Artist_Script.py
+++ Data_Related/Pokemon_TCG_Prices/TCG_Artist_Script.py
@@ -1,17 +1,18 @@
-import pandas as pd
+if __name__ == "__main__":
+    import pandas as pd
 
-fileName = "C:\\Users\\Gabriel\\Desktop\\bi_dwh pivot\\pokemon_tcg_data\\pokemon-tcg-data-master 1999-2023.csv"
-df = pd.read_csv(fileName)
+    fileName = "C:\\Users\\Gabriel\\Desktop\\bi_dwh pivot\\pokemon_tcg_data\\pokemon-tcg-data-master 1999-2023.csv"
+    df = pd.read_csv(fileName)
 
-artist_summary = (
-    df.groupby("artist")
-      .agg({
-          "id": "count",
-          "set": pd.Series.nunique,
-          "rarity": lambda x: x.value_counts().to_dict()
-      })
-      .rename(columns={"id": "Card Count", "set": "Unique Sets", "rarity":"Rarity Distribution"})
-      .reset_index()
-)
+    artist_summary = (
+        df.groupby("artist")
+        .agg({
+            "id": "count",
+            "set": pd.Series.nunique,
+            "rarity": lambda x: x.value_counts().to_dict()
+        })
+        .rename(columns={"id": "Card Count", "set": "Unique Sets", "rarity":"Rarity Distribution"})
+        .reset_index()
+    )
 
-artist_summary.to_csv("artist_summary.csv", index=False)
+    artist_summary.to_csv("artist_summary.csv", index=False)
--- Data_Related/Pokemon_TCG_Prices/TCG_Script.py
+++ Data_Related/Pokemon_TCG_Prices/TCG_Script.py
@@ -1,12 +1,3 @@
-# Import necessary libraries
-from pokemontcgsdk import Card
-from pokemontcgsdk import RestClient
-import pandas as pd
-import time
-
-# Custom api key, can be signed up for at https://dev.pokemontcg.io/
-RestClient.configure('your_api_key_goes_here')
-
 def lookup_tcg_price(card_names):
     card_data_results = []
     #list of offered TCGPlayer pricing, declared upfront for dynamic iterations
@@ -44,29 +35,41 @@ def lookup_tcg_price(card_names):
             # Optional: append the failed name for debugging
             card_data_results.append({'Name': name, 'Error': str(e)})
     return pd.DataFrame(card_data_results)
-'''
-This chunk of code can be uncommented to pull the names of the cards in the master
-dataset already in PowerBI. However, I do not want to explode my computer nor PowerBI
-by running this script on 17000 cards...
-try:
-    card_names = dataset['Name'].dropna().unique()
-except Exception as e:
-    print("Tried PowerBI dataset import, failed")
-    card_names = [
-    "Charizard", "Bulbasaur", "Gengar", "Umbreon", "Eevee",
-    "Flygon", "Gardevoir", "Metagross", "Swampert", "Snorlax",
-    "Arcanine", "Lucario", "Absol", "Blaziken", "Dragonite",
-    "Greninja", "Mimikyu", "Garchomp", "Mudkip", "Sceptile",
-    "Tyranitar", "Raichu", "Mew", "Ampharos", "Torterra"]
-'''
 
-# Top 25 most favorite Pokemon voted on Reddit in 2024
-# https://www.reddit.com/r/pokemon/comments/1bozvz5/results_favourite_pokemon_survey_2024/?rdt=61986
-card_names = [
-    "Charizard", "Bulbasaur", "Gengar", "Umbreon", "Eevee",
-    "Flygon", "Gardevoir", "Metagross", "Swampert", "Snorlax",
-    "Arcanine", "Lucario", "Absol", "Blaziken", "Dragonite",
-    "Greninja", "Mimikyu", "Garchomp", "Mudkip", "Sceptile",
-    "Tyranitar", "Raichu", "Mew", "Ampharos", "Torterra"]
-# Final output to Power BI
-result_df = lookup_tcg_price(card_names)
+
+if __name__ == "__main__":
+    # Import necessary libraries
+    from pokemontcgsdk import Card
+    from pokemontcgsdk import RestClient
+    import pandas as pd
+    import time
+
+    # Custom api key, can be signed up for at https://dev.pokemontcg.io/
+    RestClient.configure('your_api_key_goes_here')
+
+    '''
+    This chunk of code can be uncommented to pull the names of the cards in the master
+    dataset already in PowerBI. However, I do not want to explode my computer nor PowerBI
+    by running this script on 17000 cards...
+    try:
+        card_names = dataset['Name'].dropna().unique()
+    except Exception as e:
+        print("Tried PowerBI dataset import, failed")
+        card_names = [
+        "Charizard", "Bulbasaur", "Gengar", "Umbreon", "Eevee",
+        "Flygon", "Gardevoir", "Metagross", "Swampert", "Snorlax",
+        "Arcanine", "Lucario", "Absol", "Blaziken", "Dragonite",
+        "Greninja", "Mimikyu", "Garchomp", "Mudkip", "Sceptile",
+        "Tyranitar", "Raichu", "Mew", "Ampharos", "Torterra"]
+    '''
+
+    # Top 25 most favorite Pokemon voted on Reddit in 2024
+    # https://www.reddit.com/r/pokemon/comments/1bozvz5/results_favourite_pokemon_survey_2024/?rdt=61986
+    card_names = [
+        "Charizard", "Bulbasaur", "Gengar", "Umbreon", "Eevee",
+        "Flygon", "Gardevoir", "Metagross", "Swampert", "Snorlax",
+        "Arcanine", "Lucario", "Absol", "Blaziken", "Dragonite",
+        "Greninja", "Mimikyu", "Garchomp", "Mudkip", "Sceptile",
+        "Tyranitar", "Raichu", "Mew", "Ampharos", "Torterra"]
+    # Final output to Power BI
+    result_df = lookup_tcg_price(card_names)
