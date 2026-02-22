0fbb4a6205e9114c62071d0ec4eeaae4ccc963f5
Airwave-Zero <68861909+Airwave-Zero@users.noreply.github.com>
2026-02-22 15:52:14 -0800

    Normalize filter names and finalize code for looking up every player on the current leaderboards in every skill/boss

--- Data_Related/OSRS Player Behavior/gather_players_osrs.py
+++ Data_Related/OSRS Player Behavior/gather_players_osrs.py
@@ -10,10 +10,8 @@ from dataclasses import dataclass
 ============================================================
 Configuration/Initialization Functions and Variables
 ============================================================
-
-Load filter lists from external JSON so they can be edited without
-modifying the script. A default fallback is included.
-Include all necessary paths for config and output data
+Establish all global variable names and file locations here
+Establish functions for loading in configurations and returning objects that store important config types
 '''
 config_folder_dir = os.path.join(os.path.dirname(__file__), "config")
 os.makedirs(config_folder_dir, exist_ok=True)
@@ -27,152 +25,155 @@ csv_folder_path = os.path.join(os.path.dirname(__file__), "csv_data")
 os.makedirs(csv_folder_path, exist_ok=True)
 bronze_csv_folder_path = os.path.join(csv_folder_path, "raw_csv_bronze")
 silver_csv_folder_path = os.path.join(csv_folder_path, "cleaned_parquet_silver")
+
 #group_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Group_Player_List.csv")
-group_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Group_Player_List_private.csv")
 #leaderboard_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Gains_Leaderboard_Player_List.csv")
-leaderboard_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Gains_Leaderboard_Player_Listt_private.csv")
-
 #common_bot_area_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Common_Bot_Area_List.csv")
-#common_bot_area_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Common_Bot_Area_List_private.csv")
 
+# distinction between private and non private, only push non private to github
+group_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Group_Player_List_private.csv")
+leaderboard_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Gains_Leaderboard_Player_List_private.csv")
+#common_bot_area_player_list_csv_path = os.path.join(bronze_csv_folder_path, "Common_Bot_Area_List_private.csv")
 
 # object in case file doesn't exist or is unfindable
 default_filters = {
-    "skill_names": [
-        "overall",
-        "attack",
-        "strength",
-        "defence",
-        "hitpoints",
-        "ranged",
-        "prayer",
-        "magic",
-        "cooking",
-        "woodcutting",
-        "fishing",
-        "firemaking",
-        "crafting",
-        "smithing",
-        "mining",
-        "herblore",
-        "agility",
-        "thieving",
-        "slayer",
-        "farming",
-        "runecrafting",
-        "hunter",
-        "construction",
-        "sailing"
-    ],
-    "boss_hiscores": [
-        "Abyssal Sire",
-        "Alchemical Hydra",
-        "Amoxliatl",
-        "Araxxor",
-        "Artio",
-        "Barrows Chests",
-        "Bryophyta",
-        "Callisto",
-        "Calvar'ion",
-        "Cerberus",
-        "Chambers of Xeric",
-        "Chambers of Xeric: Challenge Mode",
-        "Chaos Elemental",
-        "Chaos Fanatic",
-        "Commander Zilyana",
-        "Corporeal Beast",
-        "Crazy Archaeologist",
-        "Dagannoth Prime",
-        "Dagannoth Rex",
-        "Dagannoth Supreme",
-        "Deranged Archaeologist",
-        "Doom of Mokhaiotl",
-        "Duke Sucellus",
-        "General Graardor",
-        "Giant Mole",
-        "Grotesque Guardians",
-        "Hespori",
-        "Kalphite Queen",
-        "King Black Dragon",
-        "Kraken",
-        "Kree'Arra",
-        "K'ril Tsutsaroth",
-        "Lunar Chests",
-        "Mimic",
-        "Nex",
-        "Nightmare",
-        "Obor",
-        "Phantom Muspah",
-        "Phosani's Nightmare",
-        "Sarachnis",
-        "Scorpia",
-        "Scurrius",
-        "Shellbane Gryphon",
-        "Skotizo",
-        "Sol Heredit",
-        "Spindel",
-        "Tempoross",
-        "The Gauntlet",
-        "The Corrupted Gauntlet",
-        "The Hueycoatl",
-        "The Leviathan",
-        "The Royal Titans",
-        "The Whisperer",
-        "Theatre of Blood",
-        "Theatre of Blood: Hard Mode",
-        "Thermonuclear Smoke Devil",
-        "Tombs of Amascut",
-        "Tombs of Amascut: Expert Mode",
-        "TzKal-Zuk",
-        "TzTok-Jad",
-        "Vardorvis",
-        "Venenatis",
-        "Vet'ion",
-        "Vorkath",
-        "Wintertodt",
-        "Yama",
-        "Zalcano",
-        "Zulrah"
-    ],
-    "account_types": [
-        "Normal",
-        "Ironman",
-        "Ultimate Ironman",
-        "Hardcore Ironman",
-        "Deadman Mode",
-        "Seasonal Deadman Mode",
-        "Tournament"
-    ],
-    "other_build_types": [
-        "Main",
-        "F2P",
-        "P2P",
-        "Skillers",
-        "1 Defence",
-        "Level 3",
-        "1 Hitpoint Pure",
-        "Pure",
-        "Zero Defence",
-        "Iron Pure"
-    ]
+  "skill_names": [
+    "overall",
+    "attack",
+    "strength",
+    "defence",
+    "hitpoints",
+    "ranged",
+    "prayer",
+    "magic",
+    "cooking",
+    "woodcutting",
+    "fishing",
+    "firemaking",
+    "crafting",
+    "smithing",
+    "mining",
+    "herblore",
+    "agility",
+    "thieving",
+    "slayer",
+    "farming",
+    "runecrafting",
+    "hunter",
+    "construction",
+    "sailing"
+  ],
+
+  "boss_hiscores": [
+    "abyssal_sire",
+    "alchemical_hydra",
+    "amoxliatl",
+    "araxxor",
+    "artio",
+    "barrows_chests",
+    "bryophyta",
+    "callisto",
+    "calvarion",
+    "cerberus",
+    "chambers_of_xeric",
+    "chambers_of_xeric_challenge_mode",
+    "chaos_elemental",
+    "chaos_fanatic",
+    "commander_zilyana",
+    "corporeal_beast",
+    "crazy_archaeologist",
+    "dagannoth_prime",
+    "dagannoth_rex",
+    "dagannoth_supreme",
+    "deranged_archaeologist",
+    "doom_of_mokhaiotl",
+    "duke_sucellus",
+    "general_graardor",
+    "giant_mole",
+    "grotesque_guardians",
+    "hespori",
+    "kalphite_queen",
+    "king_black_dragon",
+    "kraken",
+    "kreearra",
+    "kril_tsutsaroth",
+    "lunar_chests",
+    "mimic",
+    "nex",
+    "nightmare",
+    "obor",
+    "phantom_muspah",
+    "phosanis_nightmare",
+    "sarachnis",
+    "scorpia",
+    "scurrius",
+    "shellbane_gryphon",
+    "skotizo",
+    "sol_heredit",
+    "spindel",
+    "tempoross",
+    "the_gauntlet",
+    "the_corrupted_gauntlet",
+    "the_hueycoatl",
+    "the_leviathan",
+    "the_royal_titans",
+    "the_whisperer",
+    "theatre_of_blood",
+    "theatre_of_blood_hard_mode",
+    "thermonuclear_smoke_devil",
+    "tombs_of_amascut",
+    "tombs_of_amascut_expert",
+    "tzkal_zuk",
+    "tztok_jad",
+    "vardorvis",
+    "venenatis",
+    "vetion",
+    "vorkath",
+    "wintertodt",
+    "yama",
+    "zalcano",
+    "zulrah"
+  ],
+
+  "account_types": [
+    "normal",
+    "ironman",
+    "ultimate_ironman",
+    "hardcore_ironman"
+  ],
+
+  "other_build_types": [
+    "main",
+    "f2p",
+    "p2p",
+    "skillers",
+    "1_defence",
+    "level_3",
+    "1_hitpoint_pure",
+    "pure",
+    "zero_defence",
+    "iron_pure"
+  ]
 }
 wom_base_url = "https://api.wiseoldman.net/v2"
 
 
+# Configs relevant for running the script
 @dataclass(frozen=True)
 class ScriptConfig:
-    api_key: str
-    discord_username: str
-    request_delay: float
-    csv_output_dir_raw: str
-    csv_output_dir_processed: str
+    api_key: str # api key to raise API limits from 20 to 100 requests/minute
+    discord_username: str # user discord ign so WOM API knows who to contact if necessary
+    request_delay: float # adjustable delay to ensure we stay under 100 calls per minute
+    csv_output_dir_raw: str # directory for raw bronze data
+    csv_output_dir_processed: str # directory for cleaned silver data (parquet, etc.)
 
+# Configs relevant for filtering players and categorizing them into different buckets for analysis
 @dataclass(frozen=True)
 class FilterConfig:
-    skill_names: list
-    boss_hiscores: list
-    account_types: list
-    other_build_types: list
+    skill_names: list # e.g. overall, attack, strength, etc.
+    boss_hiscores: list # e.g. zulrah, vorkath, etc.
+    account_types: list # e.g. ironman, hardcore ironman, etc.
+    other_build_types: list # e.g. 1 defence pure, 1 hp pure, etc.
 
 def load_filters():
     '''Loads in different account filter types from default or project JSON file.
@@ -210,30 +211,24 @@ def load_script_config():
     csv_output_dir_processed = data.get("csv_output_dir_processed", "")
     return ScriptConfig(api_key, discord_username, request_delay, csv_output_dir_raw, csv_output_dir_processed)
 
+'''
+============================================================
+Getter/Fetch Functions
+============================================================
+These functions are responsible for making API calls and fetching the relevant data. 
+This includes fetching group names, players from group names, and players from the leaderboards from
+different categories. The functions are built to be reusable and modular.
+'''
 def make_wom_api_call(url: str, headers:Dict, params: Dict = None, delay_rate: float = 0.7) -> Dict:
     """
     Wrapper around requests.get with rate limiting.
     """
-    print("making api call to: ", url)
     response = requests.get(url, headers=headers, params=params)
     time.sleep(delay_rate)
 
     response.raise_for_status()
     return response.json()
 
-def write_data_to_csv(data: Dict, filepath: str):
-    """
-    Write data to a CSV file. If the file doesn't exist, it creates it and writes the header.
-    If it does exist, it appends the new data as a new row.
-    This function is generic as this can be used for players, snapshots, etc.
-    """
-    file_exists = os.path.isfile(filepath)
-
-    with open(filepath, "a", newline="", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, fieldnames=data.keys())
-        if not file_exists:
-            writer.writeheader()
-        writer.writerow(data)
 
 def fetch_group_names(headers: Dict, limit: int = 50) -> List[Dict]:
     """
@@ -263,33 +258,17 @@ def fetch_group_names(headers: Dict, limit: int = 50) -> List[Dict]:
             offset += limit
     return groups
 
-def write_groups_to_json_file(groups: List[Dict]):
-    """
-    Write groups/clans to JSON file. This list is fairly static, so this only ever
-    needs to be run once/user can delete the file if they want to refresh it.
-    """
-    if not groups:
-        return
-    with open(groupnames_path, "w", encoding="utf-8") as f:
-        json.dump(groups, f, indent=4, ensure_ascii=False)
-
-def write_group_players_to_csv(groups: List[Dict], headers: Dict, output_path: str):
-    '''This file iterates through the list of all groups, looks them up, 
-    and then writes the players into the csv that are within the current group'''
-    for group in groups:
-        group_id = group["id"]
-        url = f"{wom_base_url}/groups/{group_id}"
-        group_details = make_wom_api_call(url, headers=headers)
-        for member in group_details["memberships"]:
-            write_data_to_csv(member["player"], output_path)
-        return
-
 def fetch_current_leaderboard_names(headers: Dict, categories: List[str]) -> List[Dict]:
     """
-    Fetch top players for each category in the provided list.
-    This could be split into a method for each day, week, month, but that seems excessive
-    when they will all but put in the same datalake. Plus the returned object contains
-    the time period anyway.
+    Fetch top players for each category in the provided list. 
+    This function returns CURRENT leaderboards, not ALL TIME RECORDS leaderboards.
+    The endpoint for ALL TIME RECORDS is https://api.wiseoldman.net/v2/records/leaderboard
+
+    CURRENT TOP leaderboard for a category is: https://api.wiseoldman.net/v2/deltas/leaderboard
+
+    This function could be refactored to be modular and simply update the records/deltas endpoint, or to include
+    a specific parameter for time period, but we are only interested in current leaderboards,
+    which almost guarantees they are somewhat active.
     """
     time_periods = ["day", "week", "month"]
     leaderboard_players = []
@@ -300,26 +279,73 @@ def fetch_current_leaderboard_names(headers: Dict, categories: List[str]) -> Lis
                 "metric": category,
                 "period": time_period
             }
-            url = f"{wom_base_url}/records/leaderboard"
-            data = make_wom_api_call(url, headers=headers, params=params)
+            url = f"{wom_base_url}/deltas/leaderboard"
+            data = make_wom_api_call(url, headers=headers, params=params) # returns a list of player dicts
+            for each_player_dict in data:
+                each_player_dict["metric"] = category # add metric and period to each player dict so we know which category and time period they belong to when we write to csv
+                each_player_dict["period"] = time_period
             leaderboard_players.extend(data)
-        break
     return leaderboard_players
+'''
+============================================================
+Write Functions
+============================================================
+These functions are responsible for writing the fetched data to CSV or JSON files. 
+Most are built on top of calling write_data_to_csv since that writes every item in a list to a row
+'''
+def write_data_to_csv(data: Dict, filepath: str):
+    """
+    Write data to a CSV file. If the file doesn't exist, it creates it and writes the header.
+    If it does exist, it appends the new data as a new row.
+    This function is generic as this can be used for players, snapshots, etc.
+    """
+    file_exists = os.path.isfile(filepath)
 
-def write_exp_leaderboards_to_csv(players: List[Dict], output_path: str):
+    with open(filepath, "a", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=data.keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(data)
+
+def write_groups_to_json_file(groups: List[Dict]):
+    """
+    Write groups/clans to JSON file. This list is fairly static, so this only ever
+    needs to be run once/user can delete the file if they want to refresh it.
+    This is different than CSV because the data is more nested and doesn't fit well into a tabular format, 
+    plus we don't need to track changes over time for groups/clans, just need a reference of group names and IDs
+    """
+    if not groups:
+        return
+    with open(groupnames_path, "w", encoding="utf-8") as f:
+        json.dump(groups, f, indent=4, ensure_ascii=False)
+
+def write_group_players_to_csv(groups: List[Dict], headers: Dict, output_path: str):
+    '''This file iterates through the list of all groups, looks them up, 
+    and then writes the players into the csv that are within the current group'''
+    group_players_list = []
+    for group in groups:
+        group_id = group["id"]
+        url = f"{wom_base_url}/groups/{group_id}"
+        group_details = make_wom_api_call(url, headers=headers)
+        for member in group_details["memberships"]:
+            write_data_to_csv(member["player"], output_path)
+            group_players_list.append(member["player"])
+    return group_players_list # in case we want to do anything else with the list of players in groups after writing to csv    
+
+def write_exp_leaderboard_players_to_csv(players: List[Dict], output_path: str):
     '''This file iterates through the list of all players on the exp leaderboards, looks them up, 
     and then writes the player details into the csv'''
     for player in players:
         player_obj = player["player"]
         player_with_extrafields = {
-            "period": player["period"],
+            "startDate": player["startDate"],
+            "endDate": player["endDate"],
+            "expGained": player["gained"],
             "metric": player["metric"],
-            "value": player["value"],
-            "leaderboard_updatedAt": player["updatedAt"],
+            "period": player["period"],
             **player_obj,  # expands all player fields
         }
         write_data_to_csv(player_with_extrafields, output_path)
-
 
 def main():
     # actually handles all the control logic flow
@@ -334,10 +360,17 @@ def main():
     # particular order when querying for groups, but the same groups
     # appear everytime for the query so for our sake we will treat it as static
 
-    random_groups = fetch_group_names(wom_headers, limit=50)
-    write_groups_to_json_file(random_groups)
-    write_group_players_to_csv(random_groups, wom_headers, group_player_list_csv_path)
+    # Dataset 1: Group/Clan Players
+    #random_groups = fetch_group_names(wom_headers, limit=50)
+    #group_players = write_group_players_to_csv(random_groups, wom_headers, group_player_list_csv_path)
+    #write_groups_to_json_file(random_groups)
 
+
+    # Dataset 2: Leaderboard Players (by category and time periods)
+    # NOTE: If filtering by account type, the proper query parameter name is playerType
+    # NOTE: It is also worth breaking these writes down by time period/writing more frequently in order to 
+    # prevent issues when writing too many players and losing data, but for now we will write them all at once 
+    # and then break down by category/period in the analysis phase since there is a field for both in the csv.
     exp_leaderboard_players = fetch_current_leaderboard_names(wom_headers, account_filter_class.skill_names)
     exp_bosskc_leaderboard_players = fetch_current_leaderboard_names(wom_headers, account_filter_class.boss_hiscores)
     
@@ -345,20 +378,28 @@ def main():
     #activity_leaderboard_players = fetch_current_leaderboard_names(wom_headers, account_filter_class.activity_hiscores)
     
     # write to same leaderboard CSV since there is a field for metric and period, can drill down later
-    write_exp_leaderboards_to_csv(exp_leaderboard_players, leaderboard_player_list_csv_path)
-    write_exp_leaderboards_to_csv(exp_bosskc_leaderboard_players, leaderboard_player_list_csv_path)
+    write_exp_leaderboard_players_to_csv(exp_leaderboard_players, leaderboard_player_list_csv_path)
+    write_exp_leaderboard_players_to_csv(exp_bosskc_leaderboard_players, leaderboard_player_list_csv_path)
 
-    
+    # Dataset 3: Common Bot Area Players - this is more of a manual process where we would identify commonly botted areas 
+    # and then query the players in those hiscores and track them in a separate csv.
+    # common_botarea_players = load data from JSON
+    # write_common_botarea_players_to_csv(common_botarea_players, wom_headers, common_bot_area_player_list_csv_path)
+
     '''
-much later todo: write runelite plugin to extract names from screen when gaming in 
-commonly botted areas and add to csvv to track those players as well
+    much later todo: write runelite plugin to extract names from screen when gaming in 
+    commonly botted areas and add to csvv to track those players as well
 
-todo2: write dag/common script to essentially update/get the stats for all players
-in the separate csv's
-write functions: get_group_player_list_snapshots, get_leaderboard_player_snapshots, 
-    
+    todo2: write dag/common script to essentially update/get the stats for all players
+    in the separate csv's
+    write functions: get_group_player_list_snapshots, get_leaderboard_player_snapshots, 
+
+    # this is for getting the top of ALL TIME RECORDS leaderboard
+    https://api.wiseoldman.net/v2/records/leaderboard?metric=abyssal_sire&period=day&playerType=hardcore
+
+    # this is for getting the CURRENT TOP leaderboard for a category
+    https://api.wiseoldman.net/v2/deltas/leaderboard?metric=agility&period=day
     '''
-
 
 if __name__ == "__main__":
     # keep this simple/bare
