ee0eaa5890475149cecfdca46d8bdb62b7f7d8c4
Airwave-Zero <68861909+Airwave-Zero@users.noreply.github.com>
2026-04-06 21:22:26 -0700

    Add dagster dependencies so files run sequentially

--- Data Pipelines/OSRS Player Behavior/dagster_folder/assets/clean_snapshots.py
+++ Data Pipelines/OSRS Player Behavior/dagster_folder/assets/clean_snapshots.py
@@ -1,6 +1,7 @@
 from dagster import asset
 import clean_bronze_snapshots_parquet
+from .get_snapshots import get_snapshots
 
-@asset
+@asset(deps=[get_snapshots])
 def clean_snapshots():
     clean_bronze_snapshots_parquet.main()
---
+++ Data Pipelines/OSRS Player Behavior/dagster_folder/assets/generate_gold.py
@@ -0,0 +1,7 @@
+#from dagster import asset
+#import clean_bronze_snapshots_parquet
+#from .clean_snapshots import clean_snapshots
+
+#@asset(deps=[clean_snapshots])
+#def generate_gold():
+#    generate_gold.main()
