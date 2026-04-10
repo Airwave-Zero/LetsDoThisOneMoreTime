963ee4bbf0d5e10e615d738aa81263dfa7d88d7f
Airwave-Zero <68861909+Airwave-Zero@users.noreply.github.com>
2026-04-10 01:17:54 -0700

    Update pipeline runtime

--- Data Pipelines/OSRS Player Behavior/dagster_folder/schedules/schedule_daily.py
+++ Data Pipelines/OSRS Player Behavior/dagster_folder/schedules/schedule_daily.py
@@ -4,12 +4,14 @@ from dagster_folder.jobs.run_daily import daily_job
 # For PDT (Daylight Saving)
 daily_schedule = ScheduleDefinition(
     job=daily_job,
-    cron_schedule="0 1 * * *",  # 6 PM PDT daily
+    cron_schedule="0 1 * * *",  # 1:00 AM PST (UTC+9 conversion)
+    execution_timezone="America/Los_Angeles",  # Ensure the schedule runs in the correct timezone
 )
 '''
 # For standard PST
 daily_schedule_pst = ScheduleDefinition(
     job=daily_job,
     cron_schedule="0 2 * * *",  # 6 PM PST daily
+    execution_timezone="America/Los_Angeles",  # Ensure the schedule runs in the correct timezone   
 )
 '''
